226ba40c6597c4ab6a66a857e2d5b34bdd1bc48b
mosesglitch <mosesgachuru15@gmail.com>
2022-09-27 21:58:12 +0300

    prevent default

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
-  onInputChange(e) {
-    console.log(e.target.value);
+  //   onInputChange(e) {
+  //     console.log(e.target.value);
+  //   }
+  onFormSubmit(event) {
+    event.preventDefault();
   }
+  state = { term: "Image" };
   render() {
     return (
       <div className="io segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) =>
+                this.setState({ term: e.target.value.toUpperCase() })
+              }
+            />
+            {/* <input type="text" onChange={this.onInputChange} /> */}
             {/* <input
               type="text"
               onChange={(event) => console.log(event.target.value)}

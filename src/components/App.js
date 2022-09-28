ed49013697ae24296b1dab025e9dd3df9440c42d
mosesglitch <mosesgachuru15@gmail.com>
2022-09-28 21:45:51 +0300

    add axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log("parent class", term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID DfbRnLJVyZFOaWzdiYnIWLal9ksIdHCLlaxSxUiYU28",
+      },
+    });
   }
   render() {
     return (

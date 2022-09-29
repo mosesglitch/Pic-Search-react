b8c1fcace7c1620825b2b863466553d59cad0cde
mosesglitch <mosesgachuru15@gmail.com>
2022-09-29 11:19:03 +0300

    add ref to collect imageheight from DOM

--- src/components/ImageLIst.js
+++ src/components/ImageLIst.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageLIst = (props) => {
-  console.log(props.images);
   const images = props.images.map((image) => {
-    return <img src={image.urls.regular} />;
+    return <ImageCard key={image.id} image={image} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageLIst;
